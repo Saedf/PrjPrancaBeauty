2a799e1d8a3800722d9e7bd96ae0d727a853dfbe
Saedf <saed.fathi@gmail.com>
2024-02-20 15:28:15 +0330

    identity system

---
+++ PrjPrancaBeauty/PrancaBeauty.Infrastructure.EfCore/Context/MainContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PrancaBeauty.Domain.Users.UserAgg.Entities;
+
+namespace PrancaBeauty.Infrastructure.EfCore.Context
+{
+    public class MainContext:IdentityDbContext<Users>
+    {
+        public MainContext(DbContextOptions<MainContext> options):base(options)
+        {
+            
+        }
+    }
+}
